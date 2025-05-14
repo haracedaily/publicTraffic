fe3b01ad31cd30a7cbc9e992aedc5ad82949a17a
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-05-14 13:11:04 +0900

    no message

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import KaokaoMain from "../pages/KaokaoMain.jsx";
+import My from "../pages/My.jsx";
 
 function Main(props) {
     return (
@@ -8,6 +9,7 @@ function Main(props) {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<KaokaoMain />} />
+                    <Route path="/my" element={<My/>} />
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/pages/My.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function My(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default My;
