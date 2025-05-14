5893135fa6181722c4583a2e46cbfe88ec9cef06
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-14 13:10:42 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,35 @@ import SearchTotal from "./pages/SearchTotal.jsx";
 import BusRoute from "./pages/busRoute.jsx";
 import {useMediaQuery} from "react-responsive";
 import {Map, useKakaoLoader} from "react-kakao-maps-sdk";
+import {BrowserRouter, useNavigate} from "react-router-dom";
 
 function App() {
 
-    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isMobile = useMediaQuery({maxWidth: 768});
+    const navigator = useNavigate();
 
+    return (
+        <>
+            <header>
+                {isMobile ? <img width={150} src={"/logo_word.svg"} alt={"logo"}/> :
+                    <img width={200} src={"/header_logo.svg"} alt={'logo'}/>}
+                {isMobile ? <Button>로그인</Button> : <nav>
+                    <div onClick={()=>{navigator("/my")}}>나의버스</div>
+                    <div>주변정류장</div>
+                    <div>이용안내</div>
+                </nav>}
+                {isMobile ? <></> : <Button>로그인</Button>}
+            </header>
+            <main>
+                <Main/>
+            </main>
+            <footer>
 
-  return (
-    <>
-      <header>
-          {isMobile ?<img width={150} src={"/logo_word.svg"} alt={"logo"}/> :<img width={200} src={"/header_logo.svg"} alt={'logo'}/>}
-          {isMobile ? <Button>로그인</Button> : <nav>
-              <div>나의버스</div>
-              <div>주변정류장</div>
-              <div>이용안내</div>
-          </nav>}
-          {isMobile ? <></>:<Button>로그인</Button>}
-      </header>
-      <main>
-            <Main />
-      </main>
-      <footer>
+                <img width={200} src="/white_logo.svg" alt="footer_logo"/>
+            </footer>
+        </>
 
-          <img width={200} src="/white_logo.svg" alt="footer_logo" />
-      </footer>
-    </>
-  )
+    )
 }
 
 export default App
