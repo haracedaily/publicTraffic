ee50cf8ab620cc318854597bb5128b377d4556a8
DESKTOP-29VI7GO\user <haracedaily>
2025-05-15 13:02:42 +0900

    no message

--- src/pages/KaokaoMain.jsx
+++ src/pages/KaokaoMain.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Map, useKakaoLoader} from "react-kakao-maps-sdk";
+import {Map, MarkerClusterer, useKakaoLoader} from "react-kakao-maps-sdk";
 import Side from "../component/Side.jsx";
 import styles from "../css/kakao_main.module.css";
 function KaokaoMain(props) {
@@ -13,6 +13,12 @@ function KaokaoMain(props) {
             <article className={styles.main}>
             <Map center={{lat: 35.8596, lng:128.6028}} level={7}
                  style={{width:'100%',height:'100%'}} >
+                <MarkerClusterer
+                    averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+                    minLevel={10} // 클러스터 할 최소 지도 레벨
+                >
+
+                </MarkerClusterer>
             </Map>
             </article>
         </>
