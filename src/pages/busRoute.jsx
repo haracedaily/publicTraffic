edcdca04892297835dc2be753e704d582b9722e3
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-05-19 17:19:05 +0900

    no message

--- src/pages/busRoute.jsx
+++ src/pages/busRoute.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import {Input, Space} from "antd";
+import {Button, Input, Space} from "antd";
+import {SearchOutlined} from "@ant-design/icons";
 
 function BusRoute(props) {
     return (
         <div>
 
+            <Space.Compact style={{width: '100%', padding: '20px'}}>
+                <Input.Search placeholder="출발지를 선택해주세요." allowClear/>
+            </Space.Compact>
 
+            <Space.Compact style={{width: '100%', padding: '20px'}}>
+                <Input.Search placeholder="도착지를 선택해주세요." allowClear/>
+            </Space.Compact>
+            <div style={{  padding: '10px' }}>
+                <Button type='primary' icon={<SearchOutlined/>} style={{width: '88%'}}>
+                    경로 찾기
+                </Button>
+            </div>
         </div>
     );
 }
