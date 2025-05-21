73d3773223e4bbc82de48386efd9a6f8d8256dcc
byeonjoonsung <wnstjd637@gmail.com>
2025-05-21 17:23:49 +0900

    no message

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import KaokaoMain from "../pages/KaokaoMain.jsx";
-import MapView from '../components/MapView.jsx';
+import Nearby from '../pages/Nearby.jsx';
 
 function Main(props) {
     return (
@@ -9,6 +9,7 @@ function Main(props) {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<KaokaoMain />} />
+                    <Route path="/nearby" element={<Nearby/>}/>
                 </Routes>
             </BrowserRouter>
         </>
--- src/pages/KaokaoMain.jsx
+++ src/pages/KaokaoMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import {
   CustomOverlayMap,
   Map,
@@ -10,7 +10,6 @@ import Side from "../component/Side.jsx";
 import styles from "../css/kakao_main.module.css";
 import proj4 from "proj4";
 import MapView from "../components/MapView.jsx";
-
 
 // EPSG:5182 (TM-동부원점) 좌표계 정의
 proj4.defs(
@@ -25,10 +24,13 @@ function KaokaoMain(props) {
   const [searchResults, setSearchResults] = useState([]);
   const [arrivalInfo, setArrivalInfo] = useState(null);
   const [mapCenter, setMapCenter] = useState({ lat: 35.8693, lng: 128.6062 });
+  const [mapLevel, setMapLevel] = useState(4);
   const [selectedStop, setSelectedStop] = useState(null);
   const [hoveredStop, setHoveredStop] = useState(null);
 
   const [myPosition, setMyPosition] = useState(null);
+
+  const mapRef = useRef(null);
 
   const convertNGISToKakao = (x, y) => {
     const [longitude, latitude] = proj4("EPSG:5182", "EPSG:4326", [x, y]);
@@ -48,15 +50,19 @@ function KaokaoMain(props) {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
         const { latitude, longitude } = pos.coords;
-        setMyPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
+        setMyPosition({ lat: latitude, lng: longitude });
+        // console.log("위도:", pos.coords.latitude);
+        // console.log("경도:", pos.coords.longitude);
         setMapCenter({ lat: latitude, lng: longitude });
+        setMapLevel(4);
       },
       (err) => {
         console.error("위치 오류:", err);
-      }
+        setMyPosition(null);
+        setMapCenter({ lat: 35.8714, lng: 128.6014 });
+        setMapLevel(7);
+      },
+      { enableHighAccuracy: true, maximumAge: 0 }
     );
   }, []);
 
@@ -74,14 +80,14 @@ function KaokaoMain(props) {
       <article className={styles.main}>
         <Map
           center={mapCenter}
-          level={3}
+          level={mapLevel}
           style={{ width: "100%", height: "100%" }}
+          ref={mapRef}
         >
           <MarkerClusterer
             averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
             minLevel={10} // 클러스터 할 최소 지도 레벨
           >
-            
             {selectedStop && (
               <MapMarker
                 position={{ lat: selectedStop.lat, lng: selectedStop.lng }}
@@ -163,18 +169,18 @@ function KaokaoMain(props) {
                                 item.arrState === "전"
                                   ? "#52c41a"
                                   : item.arrState === "전전"
-                                    ? "#faad14"
-                                    : "#1890ff",
+                                  ? "#faad14"
+                                  : "#1890ff",
                               fontWeight: "bold",
                             }}
                           >
                             {item.arrState === "전"
                               ? "곧 도착"
                               : item.arrState === "전전"
-                                ? "곧 도착 예정"
-                                : item.arrState === "도착예정"
-                                  ? "차고지 대기"
-                                  : `${item.arrState} 후 도착`}
+                              ? "곧 도착 예정"
+                              : item.arrState === "도착예정"
+                              ? "차고지 대기"
+                              : `${item.arrState} 후 도착`}
                           </div>
                         </div>
                       </>
@@ -185,8 +191,22 @@ function KaokaoMain(props) {
                 </div>
               </CustomOverlayMap>
             )}
-            {myPosition && <MapView position={myPosition} />}
           </MarkerClusterer>
+            {myPosition && (
+              <MapView
+                position={myPosition}
+                onClick={() => {
+                  console.log("내 위치 클릭");
+                  if (mapRef.current && window.kakao?.maps) {
+                    const kakaoLatLng = new window.kakao.maps.LatLng(
+                      myPosition.lat,
+                      myPosition.lng
+                    );
+                    mapRef.current.setCenter(kakaoLatLng);
+                  }
+                }}
+              />
+            )}
         </Map>
       </article>
     </>
